327df8332ff996ca503d69e5710a57ab751ec9dd
kevin <441325961@qq.com>
2016-08-08 16:42:17 +0800

    去掉smartLink的返回键触发方法。

--- app/src/main/java/com/kevin/miniagv2/activity/SmartLinkActivity.java
+++ app/src/main/java/com/kevin/miniagv2/activity/SmartLinkActivity.java
@@ -1117,23 +1117,6 @@ public class SmartLinkActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    @Override
-    public boolean onKeyDown(int keyCode, KeyEvent event) {
-        if (keyCode == KeyEvent.KEYCODE_BACK
-                && event.getAction() == KeyEvent.ACTION_DOWN) {
-            if (System.currentTimeMillis() - exitTime > 2000) {
-                Toast.makeText(getApplicationContext(), "再按一次退出",
-                        Toast.LENGTH_SHORT).show();
-                exitTime = System.currentTimeMillis();
-            } else {
-                finish();
-            }
-
-            return true;
-        }
-        return super.onKeyDown(keyCode, event);
-    }
-
 }
 
 
